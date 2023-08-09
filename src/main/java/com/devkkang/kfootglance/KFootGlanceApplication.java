1e4d3e0c58d488e1d924755f5c89d1902c30c148
ksans <kssk0020@naver.com>
2023-08-09 09:57:30 +0900

    Initial commit

---
+++ src/main/java/com/devkkang/kfootglance/KFootGlanceApplication.java
@@ -0,0 +1,13 @@
+package com.devkkang.kfootglance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KFootGlanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KFootGlanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devkkang/kfootglance/KFootGlanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devkkang.kfootglance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KFootGlanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
